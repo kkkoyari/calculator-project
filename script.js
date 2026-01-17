8f001300b2c6dc0a50510ed0102290b62e3b9eee
valeryia kazyrytskaya <183213488+kkkoyari@users.noreply.github.com>
2026-01-18 00:47:43 +0300

    Initial commit: add add/subtract/multiply/divide helpers

---
+++ script.js
@@ -0,0 +1,24 @@
+function add (a, b) {
+	let result = a + b;
+	return result;
+}
+
+function subtract (a, b) {
+	let result = a - b;
+	return result;
+}
+
+function multiply (a, b) {
+	let result = a * b;
+	return result;
+}
+
+function divide (a, b) {
+	let result = a / b;
+	return result;
+}
+
+console.log(add(0.57, 89));
+console.log(subtract(0.99, 0.4));
+console.log(multiply(10, 2));
+console.log(divide(0.9, 0.3));
